ad385cac4b20ae553f375e3ad0ab69e016fe46c4
topanisto <ankaa@mit.edu>
2025-03-12 20:21:21 +0800

    initial commit :D

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod committer;
+mod timed_commitment;
+mod utils;
+mod verifier;
+
+pub use committer::*;
+pub use timed_commitment::*;
+pub use utils::*;
+pub use verifier::*;
---
+++ src/verifier.rs
@@ -0,0 +1,9 @@
+pub struct Verifier {}
+
+impl Verifier {
+    pub fn verify_commit_phase() {}
+
+    pub fn open() {}
+
+    pub fn force_open() {}
+}
