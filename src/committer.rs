075376ffc42a542ce75cf55bde9b8ca26d6ab5f4
topanisto <ankaa@mit.edu>
2025-03-12 22:45:13 +0800

    sigh working with rust ints is hard, finish commitment scheme tmrw pls

--- src/committer.rs
+++ src/committer.rs
@@ -8,8 +8,8 @@ pub struct Committer {
     p1: U256,
     p2: U256,
     pub n: NonZero<U256>,
-    pub k: u32,            // T = 2 ** k
-    pub q_array: Vec<u32>, //TODO: maybe change later?
+    pub k: u32,             // T = 2 ** k
+    pub q_array: Vec<U256>, //TODO: should this be a vector of primes?,
 }
 
 impl Committer {
@@ -26,7 +26,7 @@ impl Committer {
             }
         }
 
-        let mut p2: U256 = generate_prime(Self::BITS);
+        let mut p2: U256;
         loop {
             p2 = generate_prime(Self::BITS);
             if p2.checked_rem(&U256::from(4 as u32)).unwrap() == U256::from(3 as u32) {
@@ -36,8 +36,11 @@ impl Committer {
 
         let n = Checked::new(p1) * Checked::new(p2);
 
-        let q_array: Vec<u32> = (1..Self::BITS)
-            .filter(|&n| is_prime(&U256::from(n)))
+        let q_array: Vec<U256> = (1..Self::DEFAULT_B)
+            .filter_map(|n| match is_prime(&U256::from(n)) {
+                true => Some(U256::from(n)),
+                false => None,
+            })
             .collect();
 
         Self {
@@ -50,10 +53,43 @@ impl Committer {
         }
     }
 
-    pub fn generate_g(&self) {
-        let h = U256::random_mod(&mut OsRng, &self.n);
+    fn totient_n(&self) -> NonZero<U256> {
+        let checked_mul = (Checked::new(self.p1) - Checked::new(U256::ONE))
+            * (Checked::new(self.p2) - Checked::new(U256::ONE));
+        NonZero::new(checked_mul.0.unwrap()).unwrap()
+    }
 
-        // calculate g by cycling through primes less than B
+    pub fn generate_g(&self) -> U256 {
+        let h = U256::random_mod(&mut OsRng, &self.n);
+        let totient = self.totient_n();
+        let mut exponent = self
+            .q_array
+            .iter()
+            .fold(U256::ONE, |acc, x| acc.mul_mod(x, &totient));
+
+        let mut g = U256::ONE;
+        while exponent != U256::ZERO {
+            exponent = exponent.wrapping_sub(&U256::ONE);
+            g = g.mul_mod(&h, &self.n);
+        }
+        g
+    }
+
+    pub fn generate_u(&self, g: U256) -> U256 {
+        let totient = self.totient_n();
+        let mut a = (0..(2 as i32).pow(self.k)).fold(U256::ONE, |acc, _| {
+            acc.mul_mod(&U256::from(2 as u32), &totient)
+        });
+        let mut u = U256::ONE;
+        while a != U256::ZERO {
+            a = a.wrapping_sub(&U256::ONE);
+            u = u.mul_mod(&g, &self.n);
+        }
+        u
+    }
+
+    pub fn generate_w(&self, g: U256) //-> Vec<U256> {}
+    {
     }
 
     pub fn commit() {}
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,12 @@
+use crypto_bigint::{Checked, NonZero, U256};
 
+pub fn get_order(a: U256, p: U256, q: U256) -> U256 {
+    let checked_n = Checked::new(p) * Checked::new(q);
+    let n = checked_n.0.unwrap();
+    let checked_mul =
+        (Checked::new(p) - Checked::new(U256::ONE)) * (Checked::new(q) - Checked::new(U256::ONE));
+    let totient = checked_mul.0.unwrap();
+
+    let (q, _) = totient.div_rem(&NonZero::new(a.gcd(&n)).unwrap());
+    q
+}
